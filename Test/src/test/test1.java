aa871bb7e1f30af092982b5dfc4283c10914f5ee
ybm1968 <yby1969@naver.com>
2023-09-28 18:08:57 +0900

    ㄴㄴㄴ

---
+++ Test/src/test/test1.java
@@ -0,0 +1,8 @@
+package test;
+
+public class test1 {
+	public static void main(String[] args) {
+		System.out.println("test1 파일");
+		System.out.println("test1 파일");
+	}
+}
---
+++ Test/src/test/test2.java
@@ -0,0 +1,8 @@
+package test;
+
+public class test2 {
+	public static void main(String[] args) {
+		System.out.println("test2 파일");
+		System.out.println("test2 파일");
+	}
+}
