d14d689290801eb427122f9fb9d13713f7bc5ef5
ybm1968 <yby1969@naver.com>
2023-09-28 18:13:04 +0900

    1

--- Test/src/test/test2.java
+++ Test/src/test/test2.java
@@ -3,6 +3,6 @@ package test;
 public class test2 {
 	public static void main(String[] args) {
 		System.out.println("test2 파일");
-		System.out.println("test2 파일");
+		System.out.println("test1이 작성함");
 	}
 }
