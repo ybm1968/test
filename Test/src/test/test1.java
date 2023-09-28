8031dc02dd3b97e3f63aa0383da1f5a3b479b582
ybm1968 <yby1969@naver.com>
2023-09-28 18:17:32 +0900

    ㅇㅇ

--- Test/src/test/test1.java
+++ Test/src/test/test1.java
@@ -2,7 +2,7 @@ package test;
 
 public class test1 {
 	public static void main(String[] args) {
-		System.out.println("test1 파일");
+		System.out.println("test2가 수정함");
 		System.out.println("test2가 수정함");
 	}
 }
